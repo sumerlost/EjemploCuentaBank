8d81493d04e998f45d5af92379c9e92a05f73d1d
sumerlost <Gaby_98@hotmail.com.ar>
2023-07-04 15:46:52 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EjemploCuentaBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
