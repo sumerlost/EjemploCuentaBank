8872f4daa35f341d7c0d19f357a6862f9597a3f9
sumerlost <Gaby_98@hotmail.com.ar>
2023-07-04 22:18:04 -0300

    .

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿namespace EjemploCuentaBank
-{
+﻿using EjemploCuentaBank;
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount("<name>", 1000);
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+        // Test that the initial balances must be positive.
+        BankAccount invalidAccount;
+        try
+        {
+            invalidAccount = new BankAccount("invalid", -55);
+        }
+        catch (ArgumentOutOfRangeException e)
+        {
+            Console.WriteLine("Exception caught creating account with negative balance");
+            Console.WriteLine(e.ToString());
+            return;
         }
     }
-}
+    }
